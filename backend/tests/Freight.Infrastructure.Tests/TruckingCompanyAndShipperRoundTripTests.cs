b33761db68b753894dbbf6d29f04a0360d6baf79
Rit_the_coder <totlani.ritesh@gmail.com>
2026-08-19 22:05:33 +0200

    Removed not used test case

--- backend/tests/Freight.Infrastructure.Tests/TruckingCompanyAndShipperRoundTripTests.cs
+++ backend/tests/Freight.Infrastructure.Tests/TruckingCompanyAndShipperRoundTripTests.cs
@@ -21,41 +21,4 @@ public class TruckingCompanyAndShipperRoundTripTests : IAsyncLifetime
     }
 
     public Task DisposeAsync() => Task.CompletedTask;
-
-    [Fact]
-    public async Task TruckingCompany_PersistedAndReloaded_RoundTripsOfficeLocation()
-    {
-        var company = TruckingCompany.Create(Guid.NewGuid(), "Acme Trucking", GeoLocation.Create(52.5200, 13.4050));
-
-        await using (var writeContext = new FreightDbContext(Options()))
-        {
-            writeContext.Set<TruckingCompany>().Add(company);
-            await writeContext.SaveChangesAsync();
-        }
-
-        await using var readContext = new FreightDbContext(Options());
-        var reloaded = await readContext.Set<TruckingCompany>().FirstAsync(c => c.Id == company.Id);
-
-        Assert.Equal(company.Name, reloaded.Name);
-        Assert.Equal(company.OfficeLocation.Latitude, reloaded.OfficeLocation.Latitude, precision: 6);
-        Assert.Equal(company.OfficeLocation.Longitude, reloaded.OfficeLocation.Longitude, precision: 6);
-    }
-
-    [Fact]
-    public async Task Shipper_PersistedAndReloaded_RoundTripsContactEmail()
-    {
-        var shipper = Shipper.Create(Guid.NewGuid(), "Acme Shipping", "contact@acme.example");
-
-        await using (var writeContext = new FreightDbContext(Options()))
-        {
-            writeContext.Set<Shipper>().Add(shipper);
-            await writeContext.SaveChangesAsync();
-        }
-
-        await using var readContext = new FreightDbContext(Options());
-        var reloaded = await readContext.Set<Shipper>().FirstAsync(s => s.Id == shipper.Id);
-
-        Assert.Equal(shipper.Name, reloaded.Name);
-        Assert.Equal(shipper.ContactEmail, reloaded.ContactEmail);
-    }
 }
