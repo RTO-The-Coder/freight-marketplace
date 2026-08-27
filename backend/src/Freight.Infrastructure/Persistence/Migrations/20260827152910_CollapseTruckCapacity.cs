7a119a3ecc6062fe5a29b025785ecb54c06e6514
Rit_the_coder <totlani.ritesh@gmail.com>
2026-08-27 23:21:30 +0200

    Updated Script

---
+++ backend/src/Freight.Infrastructure/Persistence/Migrations/20260827152910_CollapseTruckCapacity.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Freight.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class CollapseTruckCapacity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TruckType",
+                table: "Trucks",
+                newName: "Type");
+
+            migrationBuilder.RenameColumn(
+                name: "TruckSize",
+                table: "Trucks",
+                newName: "Size");
+
+            migrationBuilder.RenameColumn(
+                name: "ActualPickupAt",
+                table: "Shipments",
+                newName: "EstimatedPickup");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Trucks",
+                newName: "TruckType");
+
+            migrationBuilder.RenameColumn(
+                name: "Size",
+                table: "Trucks",
+                newName: "TruckSize");
+
+            migrationBuilder.RenameColumn(
+                name: "EstimatedPickup",
+                table: "Shipments",
+                newName: "ActualPickupAt");
+        }
+    }
+}
